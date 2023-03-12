e0ddc7debea9e259be8adbe4d23b846777c632c1
Dylan Cerenov <dcerenov25@gmail.com>
2023-03-12 15:42:29 -0400

    Added architecture classes for Events

---
+++ src/routes/eventRoutes.ts
@@ -0,0 +1,5 @@
+// Event Routes 
+// Implements the routes for the event api endpoints
+// Uses a eventController object to get required data. 
+// Routes uses Controller which calls Repository which calls the DB
+
