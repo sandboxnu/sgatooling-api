0d9e5af1cfb88ceb64b7fac393aecbd52d1e1cf3
Dylan Cerenov <dcerenov25@gmail.com>
2023-03-15 15:04:00 -0400

    Fleshed out the event endpoints

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,2 +1,27 @@
 // Controller class for the Event API endpoints
 // The controller has a class and method that call the repository. 
+
+// are these imports supposed to have .js extension? 
+import HTTPError from "../errors/HTTPError.js"
+import EventsRepository from "../repository/eventRepository.js"
+import { Event } from "../types/eventType.js"
+
+const eventsRepository = new EventsRepository(); 
+
+class EventsController {
+    getAllEvents(): Event[] {
+        return eventsRepository.getAllEvents(); 
+    }
+
+    getEvent(id: string): Event {
+        const event = eventsRepository.getEvent(id);
+    
+        if (!event) {
+          throw new HTTPError("Event not found.", 400);
+        }
+    
+        return event;
+    }    
+}
+
+export default EventsController; 
