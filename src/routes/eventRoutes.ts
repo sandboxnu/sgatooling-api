f38c46f48b401885b36f62ba5e07f1271fb3094c
Dylan Cerenov <dcerenov25@gmail.com>
2023-03-29 20:23:29 -0400

    Fixed formatting

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -10,24 +10,24 @@ const router = express.Router()
 const eventsController = new EventsController();
 
 /* GET all events. */
-router.get("/", (_req, res, next) => {
-    try {
-      const eventsList = eventsController.getAllEvents();
-      res.status(200).json(eventsList);
-    } catch (err: unknown) {
-      next(err);
-    }
+router.get("/", (req, res, next) => {
+  try {
+    const eventsList = eventsController.getAllEvents();
+    res.status(200).json(eventsList);
+  } catch (err: unknown) {
+    next(err);
+  }
 });
 
 /* Gets event data for a given event */
 router.get("/:eventId", (req, res, next) => {
-    try {
-        const eventId = req.params["eventId"];
-        const event = eventsController.getEvent(eventId);
-        res.status(200).json(event);
-    } catch (err: unknown) {
-        next(err);
-    }
+  try {
+    const eventId = req.params["eventId"];
+    const event = eventsController.getEvent(eventId);
+    res.status(200).json(event);
+  } catch (err: unknown) {
+    next(err);
+  }
 });
-  
+
 module.exports = router;
