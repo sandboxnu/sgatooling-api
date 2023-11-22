d587870f41ba6b80056153ea215b3db3a24bb98c
Devashish Sood <devashishsood9@gmail.com>
2023-11-22 02:58:13 -0500

    trial 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from "./utils";
 
-const PORT = 8080;
 const app = createServer();
 
-app.listen(PORT, () => console.log(`Local Host is running on PORT: ${PORT}`));
+export default app;
+
+
