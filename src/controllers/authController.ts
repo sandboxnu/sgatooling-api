840a53b7f442f1d92e5b007e84e0641afdf5e0df
Devashish Sood <devashishsood9@gmail.com>
2023-11-23 23:51:53 -0500

    adding api kiey

---
+++ src/controllers/authController.ts
@@ -0,0 +1,7 @@
+// import passport from "passport";
+// import passportLocal from "passport-local"
+
+// class AuthController {
+//   const test = passportLocal.Strategy
+
+// }
--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,7 @@ import express from "express";
 import { membersRouter } from "./routes/memberRoutes";
 import { eventRouter } from "./routes/eventRoutes";
 import { attendanceRouter } from "./routes/attendanceRoutes";
+import { Request, Response, NextFunction } from "express";
 
 //file to export useful functions for the rest of the files/tests
 export const isEmpty = (obj: any) => {
@@ -33,12 +34,28 @@ export const createdRandomUID = (): string => {
   return uuidv4().replace(/-/g, "");
 };
 
+const authApiKey = (req: Request, res: Response, next: NextFunction) => {
+  const key = req.query.key 
+  if (key) {
+    console.log("Entering key auth")
+    next()
+  }
+  else {
+    return res.status(400).send("Incorrect API Key")
+  }
+}
+
 //const to create a server, was using to test out jest, but not going well :(,
 export const createServer = () => {
   const app = express();
 
   app.use(express.json());
   //app.use(cors());
+
+  app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+  })
 
   // Members routes
   app.use("/members", membersRouter);
@@ -51,7 +68,7 @@ export const createServer = () => {
 
   // Base route
   app.use("/", (_req, res) => {
-    res.status(404).send("Endpoint does not exist.");
+    res.status(404).send("Endpoint does not exist. This is a test");
   });
 
   return app;
