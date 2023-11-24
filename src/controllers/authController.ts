1cee71aa2b277eed4cdf2368f4f2e60a524d17f5
Devashish Sood <devashishsood9@gmail.com>
2023-11-24 18:46:38 -0500

    fixed type inconsistency between db and backend

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,10 +9,20 @@ class AuthController {
       `SELECT * FROM Member WHERE uuid = ?`,
       [id]
     );
-
+    
     try {
       console.log(memberInfo)
-      const typedUser = MemberSchema.parse(memberInfo)
+      const typedUser = MemberSchema.parse({
+        uuid: memberInfo.uuid, 
+        first_name: memberInfo.first_name, 
+        last_name: memberInfo.last_name, 
+        email: memberInfo.email, 
+        active_member: memberInfo.active_member, 
+        can_vote: memberInfo.voting_rights, 
+        include_in_quorum: memberInfo.include_in_quorum, 
+        receive_email_notifs: memberInfo.receive_not_present_email, 
+        can_log_in: !(memberInfo.sign_in_blocked)
+      })
       console.log("Parsed successfully")
       return typedUser as Member
     }
