d04444d816e7dda7c04897fa2f341bbe06f59846
Devashish Sood <devashishsood9@gmail.com>
2023-11-25 15:39:12 -0500

    converted into several svfs

--- api/attendance/getAttendanceChange.ts
+++ api/attendance/getAttendanceChange.ts
@@ -5,14 +5,10 @@ const attendanceController = new AttendanceController();
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   try {
-    const attendanceId = req.query["attendanceId"];
-    if (Array.isArray(attendanceId)) {
-      res.status(400).send("Attendance ID contains incorrect information")
-    } else {
-      const attendance = await attendanceController.getAttendanceChange(attendanceId);
+      const attendance = await attendanceController.getAttendanceChange(req.query.id as string);
       res.status(200).send(attendance);
     }
-  } catch (error) {
+    catch (error) {
     res.status(500).send("Database Error");
   }
 };
--- api/auth.ts
+++ api/auth.ts
@@ -1,51 +1,51 @@
-import express from "express"
-import passport from "passport";
-import { Strategy } from "passport-local";
-import AuthController from "../src/controllers/authController"
-import { MemberSchema } from "../src/types/types"
-import { prepareRouter } from "../src/svfUtils";
+// import express from "express"
+// import passport from "passport";
+// import { Strategy } from "passport-local";
+// import AuthController from "../src/controllers/authController"
+// import { MemberSchema } from "../src/types/types"
+// import { prepareRouter } from "../src/svfUtils";
 
 
-const authRouter = express.Router();
-const authController = new AuthController()
+// const authRouter = express.Router();
+// const authController = new AuthController()
 
 
-passport.use(new Strategy((username, password, done) => {
-  authController.getMember(username).then(
-    (data) => {
-      if (data instanceof Error) {
-        done(undefined, false, {message: 'User not found'})
-      }
-      else if (data.last_name !== password) {
-        done(undefined, false, {message: 'NUID and Last Name do not match'})
-      }
-      else {
-        done(undefined, data)
-      }
-    }
-  ).catch((err) => {
-    console.log("Error while authenticating", err)
-  })
-}))
+// passport.use(new Strategy((username, password, done) => {
+//   authController.getMember(username).then(
+//     (data) => {
+//       if (data instanceof Error) {
+//         done(undefined, false, {message: 'User not found'})
+//       }
+//       else if (data.last_name !== password) {
+//         done(undefined, false, {message: 'NUID and Last Name do not match'})
+//       }
+//       else {
+//         done(undefined, data)
+//       }
+//     }
+//   ).catch((err) => {
+//     console.log("Error while authenticating", err)
+//   })
+// }))
 
-passport.serializeUser(function(user, done) {
-  const typedUser = MemberSchema.parse(user)
-  done(undefined, typedUser.uuid)
-});
+// passport.serializeUser(function(user, done) {
+//   const typedUser = MemberSchema.parse(user)
+//   done(undefined, typedUser.uuid)
+// });
 
-passport.deserializeUser(function(id, done) {
-  authController.getMember(id as string).then((data) => {
-    done(undefined, data)
-  }).catch((err) => {
-    console.log("Error while deserializing", err)
-  })
-});
+// passport.deserializeUser(function(id, done) {
+//   authController.getMember(id as string).then((data) => {
+//     done(undefined, data)
+//   }).catch((err) => {
+//     console.log("Error while deserializing", err)
+//   })
+// });
 
-authRouter.post('/login', passport.authenticate('local'), async (req, res) => {
-  res.status(200).json({message: "Authorized"});
-})
+// authRouter.post('/login', passport.authenticate('local'), async (req, res) => {
+//   res.status(200).json({message: "Authorized"});
+// })
 
-const attendanceApp = prepareRouter(express())
+// const attendanceApp = prepareRouter(express())
 
 
-export default attendanceApp
+// export default attendanceApp
